8d82ea70c1711d00fe9f6728fca453647824810b
EsakkiVinayagam008 <esakkivinayagam001@gmail.com>
2024-05-05 18:31:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './index.css';
+import Mainpage from './Componend/Mainpage';
+
+import Calculator from './Componend/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+         
+          <Route path='/' element={<Mainpage />} />
+         
+          <Route path='/Calculator' element={<Calculator/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Componend/Mainpage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalculator } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+
+const Mainpage = () => {
+    document.body.style.background='black'
+    document.body.style.fontFamily =  'Unna, serif';
+
+    const calculator={
+        height:'400px',
+        background:'#D1C4E9',
+        width:'400px',
+        display:'inline-block',
+        position:'absolute',
+        top:'250px',
+        right:'500px'
+    }
+  return (
+    <>
+     
+ 
+<Link to="/Calculator">
+    <div style={calculator} className='hover:scale-110 transform transition-transform duration-300 rounded-lg'>
+    <FontAwesomeIcon icon={faCalculator} style={{ fontSize: '36px', width: '200px', height: '200px',paddingLeft:'100px',paddingTop:'50px' }} />
+    <h1 style={{textAlign:'center'}} className='text-3xl pt-5 font-bold '>CALCULATOR</h1>
+    </div>
+    </Link>
+    </>
+  )
+}
+
+export default Mainpage
